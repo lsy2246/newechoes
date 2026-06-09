a49cbd1e6d58d2098220bb3143957be7f417b05e
lsy <lsy22@vip.qq.com>
2026-06-09 21:07:58 +0800

    avoid slugger dependency for article routes

--- src/lib/article-route-id.js
+++ src/lib/article-route-id.js
@@ -1,4 +1,12 @@
-import { slug as githubSlug } from "github-slugger";
+const GITHUB_SLUG_STRIP_RE = /[!"#$%&'()*+,./:;<=>?@[\\\]^`{|}~]/g;
+
+function slugPathSegment(segment) {
+  return String(segment)
+    .trim()
+    .toLowerCase()
+    .replace(GITHUB_SLUG_STRIP_RE, "")
+    .replace(/\s+/g, "-");
+}
 
 export function createArticleRouteId(relativePath) {
   return String(relativePath || "")
@@ -6,7 +14,7 @@ export function createArticleRouteId(relativePath) {
     .replace(/\.(md|mdx)$/i, "")
     .split("/")
     .filter(Boolean)
-    .map((segment) => githubSlug(segment))
+    .map((segment) => slugPathSegment(segment))
     .join("/")
     .replace(/\/index$/i, "");
 }
